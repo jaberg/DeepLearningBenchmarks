ecdbadc0d10af95484144afabdc65f0308ff1ddd
James Bergstra <bergstrj@iro.umontreal.ca>
2010-04-01 00:59:33 -0400

    Mods to theano/run.sh

--- theano/run.sh
+++ theano/run.sh
@@ -6,9 +6,14 @@
 # LIBRARY_PATH to include        /u/bergstrj/pub/intel/mkl/10.2.4.032/lib/em64t
 # THEANO_FLAGS="device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def -lpthread" python mlp.py
 
-THEANO_FLAGS='device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python mlp.py
-THEANO_FLAGS='device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python mlp.py
-#THEANO_FLAGS='device=gpu0,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python mlp.py
+
+
+
+
+#THEANO_FLAGS='device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python mlp.py
+#THEANO_FLAGS='device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python mlp.py
+####THEANO_FLAGS='device=gpu0,floatX=float32' python mlp.py
 
 THEANO_FLAGS='device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python convnet.py
 THEANO_FLAGS='device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def' python convnet.py
+####THEANO_FLAGS='device=gpu0,floatX=float32' python convnet.py
