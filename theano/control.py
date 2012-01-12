b790f95da38a013ff5f1f1ba7e6b3351dfc8f1ce
Frederic <nouiz@nouiz.org>
2012-01-12 09:59:27 -0500

    Added the control gemm test in fortran order.

--- theano/control.py
+++ theano/control.py
@@ -4,7 +4,8 @@ from theano.misc.check_blas import execute
 sizes = [500, 1000, 1500, 2000, 2500]
 iters = 10
 
-for size in sizes:
-    t = execute(verbose=False, M=size, N=size, K=size, iters=iters)[0]
-    print "gemm theano{%s/%d/%d}" % (
-        theano.config.floatX, iters, size), t
+for order in ['c', 'f']:
+    for size in sizes:
+        t = execute(verbose=False, M=size, N=size, K=size, iters=iters)[0]
+        print "gemm theano{order_%s/%s/%d/%d}" % (
+            order, theano.config.floatX, iters, size), t
